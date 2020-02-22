beb53d5b285e96d86b851aba095934323d391010
Aaishika S Bhattacharya <aaishikasb@users.noreply.github.com>
2020-02-22 16:27:15 +0530

    App Updated

--- IntelliHealth/app/src/main/java/com/megahertz/intellihealth/MainActivity.kt
+++ IntelliHealth/app/src/main/java/com/megahertz/intellihealth/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.tutorialkart.webviewexample
 
 import android.os.Bundle
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,8 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       // webView.setWebViewClient(WebViewClient())
-        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(
+                view: WebView,
+                url: String
+            ) { // instead of wp-submit you can set your own element id
+                webView.loadUrl("javascript:(function(){document.getElementById('wp-submit').click();})();")
+            }
+        }
         webView.loadUrl("http://ih.ruddha.xyz")
 
     }
